72196c03745f1b1cdba6004f97282505530bc279
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-06-16 16:18:57 +0300

    added

--- innvino/resources/views/components/main-block-image.blade.php
+++ innvino/resources/views/components/main-block-image.blade.php
@@ -1,4 +1,18 @@
 <div class="main__block__image">
-    <div class="main__image"></div>
-    <div class="main__logo__desc"></div>
+    <!-----main image -->
+    <div class="main__image">
+        <img src="{{ asset('style/uploads/win2.webp') }}" alt="">
+    </div>
+    <!-----main LOGO AND DESC -->
+    <div class="main__logo__desc">
+        <div class="main__logo">
+            <img src="{{ asset('style/uploads/logo.jpg') }}" alt="">
+        </div>  
+        <div class="main__name">
+            INNVINO
+        </div>
+        <div class="main__desc">
+            <p>WINE HOUSE IS COMMITTED TO PRODUCING ESTATE GROWN WINES OF THE HIGHEST CALIBER AND DISTINCTION.</p>
+        </div>
+    </div>
 </div>
