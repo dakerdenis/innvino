96a78423671cd6a8fd6cb4e970f1ff61bf385611
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2024-06-17 12:32:59 +0300

    added

--- innvino/resources/views/layouts/app.blade.php
+++ innvino/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Innvino</title>
     <link rel="stylesheet" href="{{ asset('./style/style.css') }}">
+    
 </head>
 <body>
     <div class="main__wrapper">
