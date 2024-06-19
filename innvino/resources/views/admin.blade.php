fac177d6e5a9396fbd8ee17353b1e01847fe3644
Denis Akershteyn <dakerdenis@gmail.com>
2024-06-19 14:26:59 +0300

    added

--- innvino/resources/views/admin.blade.php
+++ innvino/resources/views/admin.blade.php
@@ -1,10 +1,38 @@
-@extends('layouts.app')
+@extends('layouts.admin-app')
 
 @section('content')
-    <h1>Admin Page</h1>
-    <p>Welcome to the admin page!</p>
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
-        <button type="submit">Logout</button>
-    </form>
+
+
+    <div class="admin__panel__wrapper">
+        <div class="admin__panel__navigation">
+            <div class="panel__navigation__logo">
+                <img src="{{asset('style/uploads/logo.jpg')}}" alt="">
+            </div>
+            <div class="panel__navigation__name">
+                Innvino 
+            </div>
+            <div class="panel__navigation__elements">
+                <a href="#">Меню</a>
+                <a href="#">Винная карта</a>
+            </div>
+            <div class="panel__navigation__logout">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            </div>
+        </div>
+
+        <div class="admin__panel__content">
+            <div class="panel__content__wrapper">
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+    
+
 @endsection
